f1037aa22432406b4d2d8b8241e9f0ac75b2d4e3
Ashwin <132899154+Ashwin-Stalin@users.noreply.github.com>
2023-09-12 12:21:38 +0530

    Update Response.java

--- DocsEditor/src/main/java/common/model/Response.java
+++ DocsEditor/src/main/java/common/model/Response.java
@@ -4,16 +4,8 @@ public class Response {
 	private String message;
 	private String error;
 	
-	public String getMessage() {
-		return this.message;
-	}
-	
 	public void setMessage(String message) {
 		this.message = message;
-	}
-
-	public String getError() {
-		return error;
 	}
 
 	public void setError(String error) {
